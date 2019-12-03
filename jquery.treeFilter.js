83494d7a7f0104ec00c51ca759f7e92c6cc39215
Roman <borodatych@demka.org>
2019-12-03 14:40:03 +0300

    Update jquery.treeFilter.js

--- jquery.treeFilter.js
+++ jquery.treeFilter.js
@@ -2,123 +2,150 @@ $.expr[':'].containsText = function(obj, index, meta, stack){
 	return $(obj).text().toLowerCase().indexOf(meta[3].toLowerCase()) >= 0;
 };
 $.fn.treeFilter = function(options) {
-	var el = this;
-	var defaults = {
-		subMenu: '.tf-submenu',
-		subMenuText: '.tf-submenu-text',
-
-		openClass: 'tf-open',
-		closeClass: 'tf-close',
-
-		subMenuDiv: '.tf-submenu-div',
-
-		menuItem: '.tf-item',
-		menuItemText: '.tf-item-text',
-		hideClass: 'tf-hide',
-		showClass: 'tf-show',
-
-		resultClass: 'tf-result',
-
-		searcher : null, /// Search Input Field
-		searchWithMenuText: true /// Search in Menu Label Items \ Default Only Menu Items Link Label
+	let el = this;
+	let defaults = {
+		menuParentRoot: '.tf-menu',			/// Общий родитель у заголовка и элементами меню
+		menuParentShow: 'tz-show',			///
+		menuParentHide: 'tz-hide',			///
+											///
+		menuTitleRoot: '.tf-submenu',		///
+		menuTitleText: '.tf-submenu-text',	///
+		menuTitleOpen: 'tf-open',			///
+		menuTitleClose: 'tf-close',			///
+											///
+		menuItemsRoot: '.tf-items',			/// Общий контейнер для всех ссылок
+		menuItemsShow: 'tf-show',			///
+		menuItemsHide: 'tf-hide',			///
+											///
+		menuItemRoot: '.tf-item',			///
+		menuItemText: '.tf-item-text',		///
+		menuItemTags: '.tf-item-tags',		/// Пока тэги пососедству с самими линками, пака небыло необходиости по другому
+		menuItemShow: 'tf-show',			///
+		menuItemHide: 'tf-hide',			///
+											///
+		resultClass: 'tf-result',			/// От найденных пунктов меню до общего родителя проставляется сквозной класс
+											///
+		searcher : null,					/// Строка поиска, сам input
+		searchWithMenuText: false			/// Искать в заголовкам \ По умолчанию ищем только в пунктах меню \ До конца еще не решил, что делать при поиске по меню: показывать всё подменю или что-то похитрее
 	};
 
 	/// Public Variables
-	var plugin = {};
-	var status = []; // save folder status for "var memory"
+	let plugin = {};
+	let status = []; // save folder status for "var memory"
 
 	plugin.settings = {};
 
 	/// Main Function
-	var init = function() {
+	let init = function() {
 		plugin.settings = $.extend({}, defaults, options);
 		plugin.el = el;
 
 		if( plugin.settings.searcher ) searcher();
+
+		memory("in");
 	};
 
 	/// PUBLIC METHOD
-	///plugin.openAll = function() { plugin.el.find("li"+plugin.settings.subMenu).addClass(plugin.settings.openClass); };
-	///plugin.closeAll = function() { plugin.el.find("li"+plugin.settings.subMenu).removeClass(plugin.settings.openClass); };
+	///plugin.openAll = function() { plugin.el.find("li"+plugin.settings.menuTitleRoot).addClass(menuTitleOpen); };
+	///plugin.closeAll = function() { plugin.el.find("li"+plugin.settings.menuTitleRoot).removeClass(menuTitleOpen); };
 
 	/// PRIVATE FUNCTION
 	/** Fired when type on search input field */
-	var searcher = function() {
-		let menuItem = plugin.settings.menuItem;
-		let menuItemText = plugin.settings.menuItemText;
+	let searcher = function() {
+		let menuParentRoot = plugin.settings.menuParentRoot;
+		let menuParentShow = plugin.settings.menuParentShow;
+		let menuParentHide = plugin.settings.menuParentHide;
 
-		let subMenu = plugin.settings.subMenu;
-		let subMenuText = plugin.settings.subMenuText;
+		let menuTitleRoot = plugin.settings.menuTitleRoot;
+		let menuTitleText = plugin.settings.menuTitleText;
+		let menuTitleOpen = plugin.settings.menuTitleOpen;
+		let menuTitleClose = plugin.settings.menuTitleClose;
+
+		let menuItemsRoot = plugin.settings.menuItemsRoot;
+		let menuItemsShow = plugin.settings.menuItemsShow;
+		let menuItemsHide = plugin.settings.menuItemsHide;
+
+		let menuItemRoot = plugin.settings.menuItemRoot;
+		let menuItemText = plugin.settings.menuItemText;
+		let menuItemTags = plugin.settings.menuItemTags;
+		let menuItemShow = plugin.settings.menuItemShow;
+		let menuItemHide = plugin.settings.menuItemHide;
+
+		let resultClass = plugin.settings.resultClass;
 
 		let clearTags = function(){
-			plugin.el.find("."+plugin.settings.resultClass).removeClass(plugin.settings.resultClass);
-			plugin.el.find(menuItem+"."+plugin.settings.hideClass).removeClass(plugin.settings.hideClass).addClass(plugin.settings.showClass);
-			plugin.el.find(subMenu+"."+plugin.settings.hideClass).removeClass(plugin.settings.hideClass).addClass(plugin.settings.showClass);
+			/// Remove Tags
+			plugin.el.find("."+resultClass).removeClass(resultClass);
+			/// Items Close
+			plugin.el.find(menuItemsRoot).removeClass(menuItemsShow).addClass(menuItemsHide);
+			/// Items Hide
+			plugin.el.find(menuItemRoot).removeClass(menuItemShow).addClass(menuItemHide);
+			/// Hide Item Root
+			plugin.el.find(menuParentRoot).removeClass(menuParentShow).addClass(menuParentHide);
+			/// Title Not Active
+			plugin.el.find(menuTitleRoot).removeClass(menuTitleOpen).addClass(menuTitleClose);
 		};
+
 		$(plugin.settings.searcher).keyup(function(){
 			let search = $(this).val();
+			clearTags();
 			if( search.length === 0 ){
-				memory("out",subMenu);
-				clearTags();
+				console.log('| .:: SEARCH ZERO keyup ::. |');
+				memory("out");
 			}
 			else{
-				/// Remove Tags
-				plugin.el.find("."+plugin.settings.resultClass).removeClass(plugin.settings.resultClass);
-
-				/// Items Hide
-				plugin.el.find(menuItem+"."+plugin.settings.showClass).removeClass(plugin.settings.showClass).addClass(plugin.settings.hideClass);
-
-				/// SubMenu Close
-				plugin.el
-					.find(subMenu+"."+plugin.settings.openClass).removeClass(plugin.settings.openClass).addClass(plugin.settings.closeClass)
-					.find(plugin.settings.subMenuDiv).hide()
-				;
-				/// SubMenu Hide
-				plugin.el
-					.find(subMenu+"."+plugin.settings.showClass).removeClass(plugin.settings.showClass).addClass(plugin.settings.hideClass);
-
-				/// Items Show by Search Input
+				/// Items Show by Search in Links Text
 				plugin.el
 					.find(menuItemText+":containsText('" + search + "')")
-					.parents(menuItem).removeClass(plugin.settings.hideClass).addClass(plugin.settings.showClass).addClass(plugin.settings.resultClass)
+					.parents(menuItemRoot).removeClass(menuItemHide).addClass(menuItemShow).addClass(resultClass)
+				;
+				/// Items Show by Search in Keyword-Tags
+				plugin.el
+					.find(menuItemTags+":containsText('" + search + "')")
+					.parents(menuItemRoot).removeClass(menuItemHide).addClass(menuItemShow).addClass(resultClass)
 				;
 
 				/// SubMenu Open/Show by Search Input
 				if( plugin.settings.searchWithMenuText ){
 					plugin.el
-						.find(subMenuText+":containsText('" + search + "')")
-						.parents(subMenu)
-						.removeClass(plugin.settings.closeClass).addClass(plugin.settings.openClass)
-						.removeClass(plugin.settings.hideClass).addClass(plugin.settings.showClass)
-						.addClass(plugin.settings.resultClass)
-						.find(plugin.settings.subMenuDiv).show()
-						.find(menuItem).removeClass(plugin.settings.hideClass).addClass(plugin.settings.showClass)
+						.find(menuTitleText+":containsText('" + search + "')")
+						.parents(menuParentRoot).find(menuTitleRoot)
+						.removeClass(menuTitleClose).addClass(menuTitleOpen)
+						.removeClass(menuItemHide).addClass(menuItemShow)
+						.addClass(resultClass)
+						///.find(menuItemsRoot).show()
+						.find(menuItemsRoot).removeClass(menuItemsHide).addClass(menuItemsShow)
+						.find(menuItemRoot).removeClass(menuItemHide).addClass(menuItemShow)
 					;
 				}
 
 				/// SubMenu Open/Show by Search Items
-				$(menuItem+"."+plugin.settings.resultClass)
-					.parents(subMenu).removeClass(plugin.settings.closeClass).addClass(plugin.settings.openClass).removeClass(plugin.settings.hideClass).addClass(plugin.settings.showClass)
-					.find(plugin.settings.subMenuDiv).show()
+				$(menuItemRoot+"."+resultClass)
+					.parents(menuParentRoot)
+					.removeClass(menuParentHide).addClass(menuParentShow)
+					.find(menuTitleRoot).removeClass(menuTitleClose).addClass(menuTitleOpen)
+					.parents(menuParentRoot)
+					.find(menuItemsRoot).removeClass(menuItemsHide).addClass(menuItemsShow)
 				;
 
 				/// Items Show by Search SubMenu
 				if( plugin.settings.searchWithMenuText ){
-					$(subMenu+"."+plugin.settings.resultClass).find(menuItem).removeClass(plugin.settings.hideClass).addClass(plugin.settings.showClass);
+					$(menuTitleRoot+"."+resultClass).find(menuItemRoot).removeClass(menuItemHide).addClass(menuItemShow);
 				}
 			}
-		}).keydown(function() {
-			if( $(this).val().length === 0 ){
-				memory("in",subMenu);
-				clearTags();
-			}
-		}).change(function() {
-			if( $(this).val().length === 0 ){
-				memory("in",subMenu);
-				clearTags();
-			}
-			else $(this).trigger('keyup')
-		});
+		})
+			//*/
+			///.keydown(function(){ $(this).trigger('change'); })
+			.change(function(){ /// Это нужно для внешней очистки поля ввода, там тригер по этому событию дергаем, что в принципе логично
+				if( $(this).val().length === 0 ){
+					console.log('| .:: SEARCH ZERO keydown\\change ::. |');
+					clearTags();
+					memory("out");
+				}
+				else $(this).trigger('keyup')
+			})
+			//*/
+		;
 	};
 	/**
 	 * Save current status of folder
@@ -126,21 +153,45 @@ $.fn.treeFilter = function(options) {
 	 * array : array that saves current status
 	 * list : el
 	 * */
-	var memory = function(action,items) {
+	let memory = function(action) {
+		console.log('| .:: MEMORY WORK ::. |',action);
+		let menuParentRoot = plugin.settings.menuParentRoot;
+		let menuParentHide = plugin.settings.menuParentHide;
+
+		let menuTitleRoot = plugin.settings.menuTitleRoot;
+		///let menuTitleText = plugin.settings.menuTitleText;
+		let menuTitleOpen = plugin.settings.menuTitleOpen;
+		let menuTitleClose = plugin.settings.menuTitleClose;
+
+		let menuItemsRoot = plugin.settings.menuItemsRoot;
+		let menuItemsShow = plugin.settings.menuItemsShow;
+		let menuItemsHide = plugin.settings.menuItemsHide;
+
+		let menuItemRoot = plugin.settings.menuItemRoot;
+		///let menuItemText = plugin.settings.menuItemText;
+		let menuItemShow = plugin.settings.menuItemShow;
+		let menuItemHide = plugin.settings.menuItemHide;
+
+		///let resultClass = plugin.settings.resultClass;
+
 		if( action === "in" ){
 			status = [];
-			plugin.el.find(items).each(function() {
-				status.push( $(this).hasClass(plugin.settings.openClass) );
+			plugin.el.find(menuItemRoot).each(function() {
+				status.push( $(this).hasClass(menuItemShow) );
 			});
 		}
 		else if ( action === "out" ){
-			plugin.el.find(items).each(function(i){
+			plugin.el.find(menuItemRoot).each(function(i){
+				let $ths = $(this);
+				$ths.removeClass(menuItemHide).parents(menuParentRoot).removeClass(menuParentHide); /// Пока тут: по умолчанию ссылки не скрыты
 				if( status[i] ){
-					$(this).addClass(plugin.settings.openClass);
+					$ths
+						.addClass(menuItemShow)
+						.parents(menuParentRoot).find(menuTitleRoot).removeClass(menuTitleClose).addClass(menuTitleOpen)
+						.parents(menuParentRoot).find(menuItemsRoot).removeClass(menuItemsHide).addClass(menuItemsShow)
+					;
 				}
-				else{
-					$(this).removeClass(plugin.settings.openClass);
-				}
+				///else $(this).removeClass(menuItemShow);
 			});
 		}
 	};
